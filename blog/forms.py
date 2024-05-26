bec3f54617c6cd8c8d2e8e6b43ea61f39af30c39
sam <sampurna.chapagainnn@gmail.com>
2024-05-27 00:22:25 +0100

    add is_featured to form fields

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class BlogCreateForm(forms.ModelForm):
     class Meta:
         model=Post
-        fields=['title', 'content']
+        fields=['title', 'content', 'is_featured']

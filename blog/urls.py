524d6ec3fc087cb5773039dabc6ae08b6837cd2b
sam <sampurna.chapagainnn@gmail.com>
2024-05-26 22:24:35 +0100

    add route for blog update

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('blog/<int:id>/', views.show, name='blog-show'),
     path('blog/create/', views.create, name='blog-create'),
+    path('blog/<int:id>/update/', views.update, name='blog-update'),
     
     
 ]

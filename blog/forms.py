a1af35537753578bc901c743b77b8db2fa1ed175
sam <sampurna.chapagainnn@gmail.com>
2024-05-27 23:50:53 +0100

    add widget to set body size in model form

--- blog/forms.py
+++ blog/forms.py
@@ -12,3 +12,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model=Comment
         fields=['body']
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 3}),
+        }

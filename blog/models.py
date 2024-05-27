a72a6ef53dcb6a6a42ba84a8c2a035ebbb18af93
sam <sampurna.chapagainnn@gmail.com>
2024-05-27 03:40:41 +0100

    add Comment model with different fields

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content=models.TextField()
@@ -12,3 +11,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post=models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment_user=models.ForeignKey(User, on_delete=models.CASCADE)
+    body=models.TextField(max_length=200)
+
+    def __str__(self):
+        return f'{self.comment_user}Comment'

8cbc21ccb88a5298452e830764045858ee0a3dda
sam <sampurna.chapagainnn@gmail.com>
2024-05-27 03:40:12 +0100

    create model form for comment

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
 class BlogCreateForm(forms.ModelForm):
     class Meta:
         model=Post
         fields=['title', 'content', 'is_featured']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields=['body']

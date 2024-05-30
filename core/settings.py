6de5c6fba65005916912b4a74aa356baf6218f9c
sam <sampurna.chapagainnn@gmail.com>
2024-05-30 18:04:01 +0100

    update login redirect url after login

--- core/settings.py
+++ core/settings.py
@@ -130,5 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-LOGIN_REDIRECT_URL='blog-home'
+LOGIN_REDIRECT_URL='blog:blog-home'
 LOGIN_URL='login'

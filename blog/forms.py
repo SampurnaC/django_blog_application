67d969aa5504219e9562d946299e176cebecdd42
sam <sampurna.chapagainnn@gmail.com>
2024-05-30 18:02:28 +0100

    add different widget attributes to blog create and comment forms

--- blog/forms.py
+++ blog/forms.py
@@ -6,12 +6,17 @@ class BlogCreateForm(forms.ModelForm):
     class Meta:
         model=Post
         fields=['title', 'content', 'is_featured']
-
+        labels = {'content': 'Body', 'is_featured': 'Is Featured'}
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'add your title here', 'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'add your content here', 'class': 'form-control'}),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model=Comment
         fields=['body']
+        labels = {'body': ''}
         widgets = {
-            'body': forms.Textarea(attrs={'rows': 3}),
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'add your comments here'}),
         }

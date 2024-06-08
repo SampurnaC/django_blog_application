52f309f5a53389b2188cfbd6742cdeecae117f28
sam <sampurna.chapagainnn@gmail.com>
2024-06-08 04:43:28 +0100

    add widget attributes to forms and add sign in form

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,55 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    
+    username = forms.CharField(widget=forms.TextInput(attrs={
+            'placeholder': 'Your username',
+            'class': 'form-control'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your Email',
+        'class': 'form-control'
+    }))
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Your Password',
+        'class': 'form-control'
+    }))
+    password2 = forms.CharField(
+        label='Password Confirmation',
+        widget=forms.PasswordInput(attrs={
+        
+        'placeholder': 'Your Password Confirmation',
+        'class': 'form-control'
+    }))
+
     class Meta:
         model=User
         fields=['username','email','password1','password2']
-
+        
+class SigninForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'form-control'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'form-control'
+    }))
+    
 class UserUpdateForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+            'placeholder': 'Your username',
+            'class': 'form-control'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your Email',
+        'class': 'form-control'
+    }))
     class Meta:
         model=User
         fields=['username','email']
@@ -19,3 +58,6 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model=Profile
         fields=['image']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
